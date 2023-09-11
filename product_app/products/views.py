d5b061f3a94b63aee96b9a5108a7e985663fd35b
Hassan-Memon <hassan.ghaddai@live.com>
2023-09-12 00:20:52 +0500

    sorting api using query param is also setup

--- product_app/products/views.py
+++ product_app/products/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import filters
 from .models import Product
 from .serializers import ProductSerializer
 
 # Create your views here.
 
+# class ProductList(generics.ListCreateAPIView):
+#     serializer_class = ProductSerializer
+
+#     def get_queryset(self):
+#         queryset = Product.objects.all()
+#         keyword = self.request.query_params.get('keyword')
+#         if keyword is not None:
+#             queryset = queryset.filter(name=keyword)
+#         return queryset
+
 class ProductList(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
-
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        keyword = self.request.query_params.get('keyword')
-        if keyword is not None:
-            queryset = queryset.filter(name=keyword)
-    
-        return queryset
+    queryset = Product.objects.all()
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name']
+    ordering_fields = '__all__'
 
 
 
