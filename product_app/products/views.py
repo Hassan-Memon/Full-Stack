653004c824168d8ed13894a6a3b3a07ed5d7dd5b
Hassan-Memon <hassan.ghaddai@live.com>
2023-09-11 23:27:30 +0500

    Product search using query params from backend impelemented

--- product_app/products/views.py
+++ product_app/products/views.py
@@ -6,8 +6,16 @@ from .serializers import ProductSerializer
 # Create your views here.
 
 class ProductList(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        keyword = self.request.query_params.get('keyword')
+        if keyword is not None:
+            queryset = queryset.filter(name=keyword)
+    
+        return queryset
+
 
 
 class ProductDetail(generics.RetrieveUpdateAPIView):
